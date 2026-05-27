30cb0c76a080ffe8a90622a2c9025296c9732f12
TearsInRain-stu <2113089981@qq.com>
2026-05-27 22:28:20 +0800

    Update openCV - read wabcon.py

--- openCV - read wabcon.py
+++ openCV - read wabcon.py
@@ -1,12 +1,16 @@
 import cv2
 
+# 打开摄像头，id = 0 为默认摄像头
 cap = cv2.VideoCapture(0)
 
-cap.set(3,1280)
-cap.set(4,960)
+# 设置摄像头窗口的宽和长
+cap.set(3,1280) # 长
+cap.set(4,960)  # 宽
 
+# 展示窗口
 while True:
-    success,img = cap.read()
-    cv2.imshow("wabcon OUTOUT",img)
+    success,img = cap.read()       # read() return(bool,picture) 
+    cv2.imshow("wabcon OUTOUT",img）
+    # 按q退出
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
