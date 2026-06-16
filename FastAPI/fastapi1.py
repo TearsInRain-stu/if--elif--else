6a365e8418f809238982bb39ce8b372bcffffa58
TearsInRain-stu <2113089981@qq.com>
2026-06-16 23:48:47 +0800

    Update and rename fastapi1 to fastapi1.py

---
+++ FastAPI/fastapi1.py
@@ -0,0 +1,32 @@
+"""
+书名：不能为空，长度2——20
+作者：长度：2——10
+出版社：默认值“黑马出版社
+售价：不能为空；价格大于0元
+"""
+
+
+from fastapi import FastAPI
+from pydantic import BaseModel,Field
+
+app = FastAPI()
+
+class Book(BaseModel):
+    title: str = Field(...,min_length=2,max_length=20,description="用户名，长度要求在2~20")
+    author: str = Field(...,min_length=2,max_length=10)
+    publisher: str = Field("黑马程序员")
+    price: int = Field(...,ge=0)
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello":"World"}
+
+@app.post("/book")
+async def new_book(book:Book):
+    return book
+
+
+
+
+
